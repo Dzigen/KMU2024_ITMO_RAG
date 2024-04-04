de40ea4a579c71fe38c955c03f5a261d8e5f7dcd
dizgen <dzigen@gmail.com>
2024-04-05 01:21:25 +0300

    init repo

---
+++ src/colbert_model.py
@@ -0,0 +1,147 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from transformers import AutoModel, AutoTokenizer
+
+BASE_COLBERT_ENCODER = 'FacebookAI/roberta-base'
+OUT_TOKEN_DIM = 768
+
+ColBertTokenizer = AutoTokenizer.from_pretrained(BASE_COLBERT_ENCODER)
+
+class ColBERT(nn.Module):
+    def __init__(self, candidates, reduced_dim, base_model_path=BASE_COLBERT_ENCODER):
+        super(ColBERT, self).__init__()
+
+        self.query_encoder = AutoModel.from_pretrained(base_model_path)
+        self.query_dim_reduce = nn.Linear(768, reduced_dim)
+
+        self.document_encoder = AutoModel.from_pretrained(base_model_path)
+        self.document_dim_reduce = nn.Linear(768, reduced_dim)
+
+        self.n_cands = candidates
+
+    def forward(self, q_ids, q_masks, d_ids, d_masks):
+        '''
+        params:
+            q_ids: BxL
+            q_masks: BxL
+            d_ids: NxL
+            d_masks: NxL
+        
+        output:
+            scores: BxN
+        '''
+        print("=='colbert foward'-func")
+        print("q_ids - ", q_ids.shape)
+        print("q_masks - ", q_masks.shape)
+        print("d_ids - ", d_ids.shape)
+        print("d_masks - ", d_masks.shape)
+        print()
+
+        encoded_queries = self.q_enc(q_ids, q_masks)
+        print("ecoded queries = ", encoded_queries.shape)
+
+        encoded_documents = self.d_enc(d_ids, d_masks)
+        print("encoded documents = ", encoded_documents.shape)
+
+        scores = self.compute_scores(
+            encoded_queries, q_masks, encoded_documents, d_masks)
+        print("out scores = ", scores.shape)
+
+        return scores
+
+
+    def q_enc(self, q_ids, q_masks):
+        '''
+        params:
+            q_ids: BxL
+            q_mask: BxL
+
+        output:
+            query_embeddings: BxLxK
+        '''
+        print("=='q_enc'-function: ")
+        print("q_ids - ", q_ids.shape)
+        print("q_masks - ", q_masks.shape)
+        print()
+
+        print("out:")
+        Q = self.query_encoder(q_ids, attention_mask=q_masks).last_hidden_state
+        print("encoder -", Q.shape)
+        Q = self.query_dim_reduce(Q)
+        print("dim reduce -", Q.shape)
+
+        return nn.functional.normalize(Q, dim=-1)
+
+    def d_enc(self, d_ids, d_masks):
+        '''
+        params:
+            d_ids: NxL
+            d_mask: NxL
+
+        output:
+            document_embeddings: BxNxLxK
+        '''
+        print("=='d_enc'-function: ")
+        print("d_ids - ", d_ids.shape)
+        print("d_masks - ", d_masks.shape)
+        print()
+
+        bsz, total_length = d_ids.shape[0], d_ids.shape[1]
+
+
+        print("out: ")
+        D = self.query_encoder(d_ids, attention_mask=d_masks).last_hidden_state
+        print("encoder - ", D.shape)
+        D = self.query_dim_reduce(D)
+        norm_d = nn.functional.normalize(D, dim=-1)
+        print("dim reduce - ", D.shape)
+        #NxLxK
+
+        return norm_d
+
+    def compute_scores(self, q_hidden, q_mask, d_hidden, d_mask):
+        '''
+        params:
+            q_hidden: BxLxK
+            q_mask: BxL
+            d_hidden: NxLxK
+            d_mask: NxL
+
+        output:
+            scores: BxN
+        '''
+        print("=='compute_score'-function: ")
+        print('q_hidden - ', q_hidden.shape)
+        print('q_mask - ', q_mask.shape)
+        print('d_hidden - ', d_hidden.shape)
+        print('d_mask - ', d_mask.shape)
+        print()
+
+
+        batch_scores = torch.tensor([], requires_grad=True)
+        for i in range(q_hidden.shape[0]):
+            C = F.cosine_similarity(q_hidden[i].unsqueeze(1).unsqueeze(1), 
+                                    d_hidden, dim=-1)
+            C = C.permute((1,0,2))
+            print("cos similarity -",C.shape)
+
+            d_masked_C = C.masked_fill(~d_mask.unsqueeze(1).bool(), -100)
+            print("mask doc tokens - ",d_masked_C.shape)
+
+            max_C = d_masked_C.max(dim=-1).values
+            print("find max sim - ", max_C.shape)
+
+            q_masked_C = max_C.masked_fill(~q_mask[i].unsqueeze(0).bool(), 0)
+            print("mask query tokens - ",q_masked_C.shape)
+
+            scores = q_masked_C.sum(dim=-1)
+            print("sum scores - ",scores.shape)
+
+            batch_scores = torch.cat((batch_scores, scores.unsqueeze(0)), dim=0)
+
+        return batch_scores
+
+
+
+
---
+++ src/fid_model.py
@@ -0,0 +1,177 @@
+
+
+
+import types
+import torch
+import transformers
+import torch.nn.functional as F
+from torch import nn
+
+class FiDT5(transformers.T5ForConditionalGeneration):
+    def __init__(self, config):
+        super().__init__(config)
+        self.wrap_encoder()
+
+    def forward_(self, **kwargs):
+        #print("forward 1")
+
+        if 'input_ids' in kwargs:
+            kwargs['input_ids'] = kwargs['input_ids'].view(kwargs['input_ids'].size(0), -1)
+        if 'attention_mask' in kwargs:
+            kwargs['attention_mask'] = kwargs['attention_mask'].view(kwargs['attention_mask'].size(0), -1)
+
+        return super(FiDT5, self).forward(
+            **kwargs
+        )
+
+    # We need to resize as B x (N * L) instead of (B * N) x L here
+    # because the T5 forward method uses the input tensors to infer
+    # dimensions used in the decoder.
+    # EncoderWrapper resizes the inputs as (B * N) x L.
+    def forward(self, input_ids=None, attention_mask=None, **kwargs):
+        #print("forward 2")
+
+        # BxNxL
+        #print("init dimension: ", input_ids.shape if input_ids is not None else input_ids)
+
+        if input_ids != None:
+            # inputs might have already be resized in the generate method
+            if input_ids.dim() == 3:
+                self.encoder.n_passages = input_ids.size(1)
+            input_ids = input_ids.view(input_ids.size(0), -1)
+        if attention_mask != None:
+            attention_mask = attention_mask.view(attention_mask.size(0), -1)
+
+        # BxN*L
+        #print("2d resize: ", input_ids.shape if input_ids is not None else input_ids)
+
+        return super().forward(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            **kwargs
+        )
+
+    # We need to resize the inputs here, as the generate method expect 2D tensors
+    def generate(self, input_ids, attention_mask, max_length):
+        #print("generate")
+
+        self.encoder.n_passages = input_ids.size(1)
+        
+        # BxNxL
+        #print("init dimension",input_ids.shape)
+
+        return super().generate(
+            input_ids=input_ids.view(input_ids.size(0), -1),
+            attention_mask=attention_mask.view(attention_mask.size(0), -1),
+            max_length=max_length
+        )
+
+    def wrap_encoder(self, use_checkpoint=False):
+        """
+        Wrap T5 encoder to obtain a Fusion-in-Decoder model.
+        """
+        self.encoder = EncoderWrapper(self.encoder, use_checkpoint=use_checkpoint)
+
+    def unwrap_encoder(self):
+        """
+        Unwrap Fusion-in-Decoder encoder, useful to load T5 weights.
+        """
+        self.encoder = self.encoder.encoder
+        block = []
+        for mod in self.encoder.block:
+            block.append(mod.module)
+        block = nn.ModuleList(block)
+        self.encoder.block = block
+
+    def load_t5(self, state_dict):
+        self.unwrap_encoder()
+        self.load_state_dict(state_dict)
+        self.wrap_encoder()
+
+class EncoderWrapper(torch.nn.Module):
+    """
+    Encoder Wrapper for T5 Wrapper to obtain a Fusion-in-Decoder model.
+    """
+    def __init__(self, encoder, use_checkpoint=False):
+        super().__init__()
+
+        self.encoder = encoder
+        self.main_input_name = 'input_ids'
+        apply_checkpoint_wrapper(self.encoder, use_checkpoint)
+
+    def forward(self, input_ids=None, attention_mask=None, **kwargs,):
+        #print("forward 3")
+        # BxN*L
+        #print("2d resize: ",input_ids.shape)
+        #print("---")
+
+        # total_length = n_passages * passage_length
+        bsz, total_length = input_ids.shape
+        passage_length = total_length // self.n_passages
+        input_ids = input_ids.view(bsz*self.n_passages, passage_length)
+        attention_mask = attention_mask.view(bsz*self.n_passages, passage_length)
+        
+        # B*NxL
+        #print("candidates flat: ",input_ids.shape)
+
+        outputs = self.encoder(input_ids, attention_mask, **kwargs)
+        
+        # B*NxLx768
+        #print("encoder output ",outputs[0].shape)
+        
+        
+        #print("candidates concatenation:")
+        for key in outputs.keys():
+            # BxN*Lx768
+            outputs[key] = outputs[key].view(bsz, self.n_passages*passage_length, -1)
+            #print(key, outputs[key].shape)
+        
+        return outputs
+
+class CheckpointWrapper(torch.nn.Module):
+    """
+    Wrapper replacing None outputs by empty tensors, which allows the use of
+    checkpointing.
+    """
+    def __init__(self, module, use_checkpoint=False):
+        super().__init__()
+        self.module = module
+        self.use_checkpoint = use_checkpoint
+
+    def forward(self, hidden_states, attention_mask, position_bias, **kwargs):
+        if self.use_checkpoint and self.training:
+            kwargs = {k: v for k, v in kwargs.items() if v is not None}
+            def custom_forward(*inputs):
+                output = self.module(*inputs, **kwargs)
+                empty = torch.tensor(
+                    [],
+                    dtype=torch.float,
+                    device=output[0].device,
+                    requires_grad=True)
+                output = tuple(x if x is not None else empty for x in output)
+                return output
+
+            output = torch.utils.checkpoint.checkpoint(
+                custom_forward,
+                hidden_states,
+                attention_mask,
+                position_bias
+            )
+            output = tuple(x if x.size() != 0 else None for x in output)
+        else:
+            output = self.module(hidden_states, attention_mask, position_bias, **kwargs)
+        return output
+
+def apply_checkpoint_wrapper(t5stack, use_checkpoint):
+    """
+    Wrap each block of the encoder to enable checkpointing.
+    """
+    block = []
+    for mod in t5stack.block:
+        wrapped_mod = CheckpointWrapper(mod, use_checkpoint)
+        block.append(wrapped_mod)
+    block = nn.ModuleList(block)
+    t5stack.block = block
+
+
+
